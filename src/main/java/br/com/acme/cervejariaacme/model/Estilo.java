9598f0fc2588c4b317da90963705f6b0a77ae2f9
leonardogloria <contato@leonardogloria.com.br>
2026-03-24 21:50:20 -0300

    primeiro build

--- src/main/java/br/com/acme/cervejariaacme/model/Estilo.java
+++ src/main/java/br/com/acme/cervejariaacme/model/Estilo.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+//Alterei essa classe
 public class Estilo {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
